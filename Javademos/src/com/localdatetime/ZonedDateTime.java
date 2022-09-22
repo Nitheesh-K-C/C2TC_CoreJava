dc73f7f7b94be7bbc9993f16de3c65fde3200cc9
NITHEESH <NITHEESH@LAPTOP-C74ME5K4>
2022-09-22 19:09:53 +0530

    commit

---
+++ Javademos/src/com/localdatetime/ZonedDateTime.java
@@ -0,0 +1,21 @@
+package com.localdatetime;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Set;
+
+public class ZonedDateTime {
+
+	public static void main(String[] args) {
+		
+		ZoneId zoneId = ZoneId.of("Asia/Tokyo");
+		
+	/*	Set<String> availableZoneIds = ZoneId.getAvailableZoneIds();
+		availableZoneIds.forEach(System.out::println);
+*/
+
+        ZonedDateTime zonedDateTime =  zonedDateTime.of
+         
+	}
+
+}
